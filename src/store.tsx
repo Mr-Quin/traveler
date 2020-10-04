b85dd54ec81b6af73ab6de3b88be8246569547de
Mr-Quin <hr82003@gmail.com>
2020-10-03 21:04:03 -0700

    Added some more default states

--- src/store.tsx
+++ src/store.tsx
@@ -2,11 +2,15 @@ import create from 'zustand'
 import * as THREE from 'three'
 
 type State = {
-    prismColorMain: THREE.Color
-    prismColorSub: THREE.Color
+    prismColorPrimary: THREE.Color
+    prismColorSecondary: THREE.Color
     prismPosition: THREE.Vector3
+    prismScale: THREE.Vector3
     ringColor: THREE.Color
     groundColor: THREE.Color
+    marsPosition: THREE.Vector3
+    moon1Position: THREE.Vector3
+    moon2Position: THREE.Vector3
     glRenderer: THREE.WebGLRenderer | null
     effectsEnabled: boolean
     quality: number
@@ -18,11 +22,15 @@ type State = {
 }
 
 const useStore = create<State>((set, get) => ({
-    prismColorMain: new THREE.Color('#355cd6'),
-    prismColorSub: new THREE.Color('#6cbca0'),
+    prismColorPrimary: new THREE.Color('#355cd6'),
+    prismColorSecondary: new THREE.Color('#6cbca0'),
     prismPosition: new THREE.Vector3(0, 8, 0),
+    prismScale: new THREE.Vector3(1, 1.8, 1),
     ringColor: new THREE.Color('#d7ee9b'),
     groundColor: new THREE.Color('#555555'),
+    marsPosition: new THREE.Vector3(250, 500, -600),
+    moon1Position: new THREE.Vector3(-650, 5, -400),
+    moon2Position: new THREE.Vector3(100, -50, 400),
     glRenderer: null,
     effectsEnabled: true,
     quality: window.devicePixelRatio,
