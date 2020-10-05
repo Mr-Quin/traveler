c3d352d6b2c7d21a841338f53bc6d9556c55fbb0
Mr-Quin <hr82003@gmail.com>
2020-10-04 17:49:47 -0700

    Added some todo

--- src/hooks/useOrbit.tsx
+++ src/hooks/useOrbit.tsx
@@ -12,6 +12,11 @@ type T = (
 ) => React.MutableRefObject<any>
 
 const useOrbit: T = (ref, targetRef, { axis = new THREE.Vector3(0, 1, 0), rate = 0.01 }) => {
+    /*
+     TODO: Provide another point to calculate rotation axis
+           The rotational axis is perpendicular to the plane constructed by ref, targetRef, and another Vector3
+     */
+
     useFrame(() => {
         ref.current!.position.sub(targetRef.current!.position)
         ref.current!.position.applyAxisAngle(axis!, rate!)
