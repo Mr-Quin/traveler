197e75b0dfde835166c8f8b1946063a54df476ad
Mr-Quin <hr82003@gmail.com>
2020-10-05 00:15:45 -0700

    remove unused imports

--- src/components/Sound.tsx
+++ src/components/Sound.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { useAudioStore } from '../store'
 
 const ambientAudio = require('../assets/ambient.ogg') // using import causes error in ts
