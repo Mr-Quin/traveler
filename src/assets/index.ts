f01cfeb5d6efa250555390435c726b591ecf2298
Mr-Quin <hr82003@gmail.com>
2020-10-24 01:33:45 -0700

    export assets with index file

---
+++ src/assets/index.ts
@@ -0,0 +1,22 @@
+import ambientSound from './sounds/ambient.ogg'
+import sunTex from './textures/2k_sun.jpg'
+import marsTex from './textures/2k_makemake_fictional.jpg'
+import moon1Tex from './textures/2k_ceres_fictional.jpg'
+import moon2Tex from './textures/2k_haumea_fictional.jpg'
+
+import lensFlare from './textures/lens_flare.jpg'
+import lensFlareDisk from './textures/lens_flare_disk.jpg'
+import lensFlareBlur from './textures/lens_flare_blur.jpg'
+import lensFlareRing from './textures/lens_flare_ring.jpg'
+
+export {
+    ambientSound,
+    sunTex,
+    marsTex,
+    moon1Tex,
+    moon2Tex,
+    lensFlare,
+    lensFlareDisk,
+    lensFlareBlur,
+    lensFlareRing,
+}
