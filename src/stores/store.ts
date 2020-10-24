f1514173d8f7edd1a7f121449fe1ee20af11e7fb
Mr-Quin <hr82003@gmail.com>
2020-10-23 17:04:17 -0700

    move files to another directory

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -2,16 +2,14 @@ import create from 'zustand'
 import * as THREE from 'three'
 
 type State = {
+    readonly statics: {}
     prismColorPrimary: THREE.Color
     prismColorSecondary: THREE.Color
     prismPosition: THREE.Vector3
     cameraPosition: THREE.Vector3
     prismScale: THREE.Vector3
     ringColor: THREE.Color
-    groundColor: THREE.Color
-    marsPosition: THREE.Vector3
-    moon1Position: THREE.Vector3
-    moon2Position: THREE.Vector3
+    fov: number
     glRenderer: THREE.WebGLRenderer | null
     isMobile: boolean
     effectsEnabled: boolean
@@ -24,16 +22,14 @@ type State = {
 }
 
 const useStore = create<State>((set, get) => ({
+    statics: {},
     prismColorPrimary: new THREE.Color('#355cd6'),
     prismColorSecondary: new THREE.Color('#6cbca0'),
     prismPosition: new THREE.Vector3(0, 8, 0),
     cameraPosition: new THREE.Vector3(0, 1.6, 20),
     prismScale: new THREE.Vector3(1, 1.8, 1),
     ringColor: new THREE.Color('#d7ee9b'),
-    groundColor: new THREE.Color('#555555'),
-    marsPosition: new THREE.Vector3(250, 500, -600),
-    moon1Position: new THREE.Vector3(-650, 5, -400),
-    moon2Position: new THREE.Vector3(100, -50, 400),
+    fov: 73.7,
     glRenderer: null,
     isMobile: /iPhone|iPad|iPod|Android/i.test(navigator.userAgent),
     effectsEnabled: true,
@@ -45,15 +41,4 @@ const useStore = create<State>((set, get) => ({
     },
 }))
 
-type AudioState = {
-    audio: boolean
-    toggleAudio: () => void
-}
-
-const useAudioStore = create<AudioState>((set, get) => ({
-    audio: false,
-    toggleAudio: () => void set({ audio: !get().audio }),
-}))
-
 export default useStore
-export { useAudioStore }
