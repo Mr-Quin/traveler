6aff7c53f49766042ff2bd43d40ef28e7282247d
Mr-Quin <hr82003@gmail.com>
2020-10-24 01:34:49 -0700

    rename type

--- src/hooks/useWobble.tsx
+++ src/hooks/useWobble.tsx
@@ -1,17 +1,17 @@
 import { useFrame } from 'react-three-fiber'
 import React from 'react'
 
-type T = (
+type Wobble = (
     ref: React.MutableRefObject<any>,
     options: {
         readonly axis?: string
         readonly factor?: number
         readonly defaultValue?: number
-        readonly fn?: string
+        readonly fn?: 'sin' | 'tan' | 'cos'
     }
 ) => React.MutableRefObject<any>
 
-const useWobble: T = (ref, { axis = 'y', factor = 1, defaultValue = 0, fn = 'sin' }) => {
+const useWobble: Wobble = (ref, { axis = 'y', factor = 1, defaultValue = 0, fn = 'sin' }) => {
     useFrame((state) => {
         const time = state.clock.getElapsedTime()
         ref.current!.position[axis] = defaultValue + Math[fn](time) * factor
